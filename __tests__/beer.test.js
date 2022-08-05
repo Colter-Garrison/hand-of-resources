0c6e3f140b958b2539e6dec6bebf7695127d57ed
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-05 16:35:27 -0700

    16th test passing. #GET /beer.

---
+++ __tests__/beer.test.js
@@ -0,0 +1,61 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET /beers should return a list of beers', async () => {
+    const resp = await request(app).get('/beers');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'Party Time Pils',
+        style: 'German Pilsner',
+        producer: 'Wayfinder Beer',
+        location: 'Portland, OR'
+      },
+      {
+        id: '2',
+        name: 'El Guapo',
+        style: 'Blonde Ale',
+        producer: 'New Spring Brewing',
+        location: 'Albany, OR'
+      },
+      {
+        id: '3',
+        name: 'Dry Stout',
+        style: 'Irish Stout',
+        producer: 'Ferment',
+        location: 'Hood River, OR'
+      },
+      {
+        id: '4',
+        name:  'Maibock',
+        style: 'Hell/Maibock',
+        producer:  'Buoy Beer Company',
+        location:  'Astoria, OR'
+      },
+      {
+        id:  '5',
+        name: 'DREAMLAND',
+        style: 'Sour',
+        producer: 'Black Project',
+        location: 'Denver, CO'
+      },
+      {
+        id: '6',
+        name: 'Orange Gose',
+        style: 'Fruited Gose',
+        producer: 'Pohjala',
+        location: 'Tallinn, Harjumaa Estonia'
+      },
+    ]);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
