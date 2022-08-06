fe780b255a454479ecfb6704cf65b9d090151ea8
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-05 17:08:17 -0700

    21st test passing. #GET /animals.

---
+++ __tests__/animals.test.js
@@ -0,0 +1,44 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET /animals should return a list of animals', async () => {
+    const resp = await request(app).get('/animals');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'Momo',
+        type: 'Cat',
+        color: 'Black',
+      },
+      {
+        id: '2',
+        name: 'Stu',
+        type: 'Cat',
+        color: 'White & Grey',
+      },
+      {
+        id: '3',
+        name: 'Ghoul',
+        type: 'Cat',
+        color: 'Black',
+      },
+      {
+        id: '4',
+        name: 'Ozujsko',
+        type: 'Dog',
+        color: 'White & Brown',
+      },
+    ]);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
